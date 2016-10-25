72ee33f7bc82cb1a6529e8f468b4b3082642c24f
Ryan Wade <ryan.vbprgmr@gmail.com>
2016-10-25 16:39:31 -0500

    Add Components to index.js export

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var Button  = require('./dist/components/Button.js');
-var Title   = require('./dist/components/Title.js');
+var Button      = require('./dist/components/Button.js');
+var Title       = require('./dist/components/Title.js');
+var Divider     = require('./dist/components/Divider.js');
+var NumberField = require('./dist/components/NumberField.js');
+var TextField   = require('./dist/components/TextField.js');
+var TextArea    = require('./dist/components/TextArea.js');
 
 module.exports = {
     Button,
-    Title
+    Title,
+    Divider,
+    NumberField,
+    TextField,
+    TextArea
 };
