f7045fd28696e50dff5950fe38fbc2d3b77e733c
Ryan Wade <ryan.vbprgmr@gmail.com>
2016-10-25 17:00:25 -0500

    Fixes #1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var Button      = require('./dist/components/Button.js');
-var Title       = require('./dist/components/Title.js');
-var Divider     = require('./dist/components/Divider.js');
-var NumberField = require('./dist/components/NumberField.js');
-var TextField   = require('./dist/components/TextField.js');
-var TextArea    = require('./dist/components/TextArea.js');
+var Button      = require('./dist/components/Button.js').default;
+var Title       = require('./dist/components/Title.js').default;
+var Divider     = require('./dist/components/Divider.js').default;
+var NumberField = require('./dist/components/NumberField.js').default;
+var TextField   = require('./dist/components/TextField.js').default;
+var TextArea    = require('./dist/components/TextArea.js').default;
 
 module.exports = {
     Button,
