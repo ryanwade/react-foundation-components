7f52cb800341a9128cd804d19b2e851d0208673b
Ryan Wade <ryan.vbprgmr@gmail.com>
2016-10-29 13:51:43 -0500

    Add Expand Component

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var Button      = require('./dist/components/Button.js').default;
-var Title       = require('./dist/components/Title.js').default;
-var Divider     = require('./dist/components/Divider.js').default;
-var NumberField = require('./dist/components/NumberField.js').default;
-var TextField   = require('./dist/components/TextField.js').default;
-var TextArea    = require('./dist/components/TextArea.js').default;
+var Button          = require('./dist/components/Button.js').default;
+var Title           = require('./dist/components/Title.js').default;
+var Divider         = require('./dist/components/Divider.js').default;
+var NumberField     = require('./dist/components/NumberField.js').default;
+var TextField       = require('./dist/components/TextField.js').default;
+var TextArea        = require('./dist/components/TextArea.js').default;
+
+var Expand          = require('./dist/components/Expand/Expand.js').default;
+var ExpandTitle     = require('./dist/components/Expand/ExpandTitle.js').default;
+var ExpandContent   = require('./dist/components/Expand/ExpandContent.js').default;
 
 module.exports = {
     Button,
@@ -11,5 +15,8 @@ module.exports = {
     Divider,
     NumberField,
     TextField,
-    TextArea
+    TextArea,
+    Expand,
+    ExpandTitle,
+    ExpandContent
 };
