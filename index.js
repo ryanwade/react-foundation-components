d11f1983a82c416973f1b58cbeb8b8dd1f1f6033
Ryan Wade <ryan.vbprgmr@gmail.com>
2016-11-12 01:36:17 -0600

    Add components to export and alphebetize

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var Button          = require('./dist/components/Button.js').default;
-var Title           = require('./dist/components/Title.js').default;
 var Divider         = require('./dist/components/Divider.js').default;
-var NumberField     = require('./dist/components/NumberField.js').default;
-var TextField       = require('./dist/components/TextField.js').default;
-var TextArea        = require('./dist/components/TextArea.js').default;
 var Expand          = require('./dist/components/Expand.js').default;
+var Icon            = require('./dist/components/Icon.js').default;
+var Menu            = require('./dist/components/Menu.js').default;
+var MenuItem        = require('./dist/components/MenuItem.js').default;
+var MenuText        = require('./dist/components/MenuText.js').default;
+var NumberField     = require('./dist/components/NumberField.js').default;
+var Row             = require('./dist/components/Row.js').default;
+var TextArea        = require('./dist/components/TextArea.js').default;
+var TextField       = require('./dist/components/TextField.js').default;
+var Title           = require('./dist/components/Title.js').default;
 
 module.exports = {
     Button,
-    Title,
     Divider,
+    Expand,
+    Icon,
+    Menu,
+    MenuItem,
+    MenuText,
     NumberField,
-    TextField,
+    Row,
     TextArea,
-    Expand
+    TextField,
+    Title
 };
