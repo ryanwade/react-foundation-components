6a3c4c09ea4bd50323a2e2af40a6289dead180f1
Ryan Wade <ryan.wade.developer@outlook.com>
2017-06-05 20:21:53 -0500

    react router path fix

--- docs_src/src/routes.js
+++ docs_src/src/routes.js
@@ -7,7 +7,7 @@ import AboutPage from './components/AboutPage';
 import NotFoundPage from './components/NotFoundPage';
 
 export default (
-  <Route path="/" component={App}>
+  <Route path="/react-foundation-components/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
